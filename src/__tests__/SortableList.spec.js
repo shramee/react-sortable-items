a1c845c0fd3d1cfed51160175f2ba2f095f0773e
Shramee Srivastav <shramee.srivastav@gmail.com>
2020-06-27 16:36:18 +0800

    Fixing tests

--- src/__tests__/SortableList.spec.js
+++ src/__tests__/SortableList.spec.js
@@ -1,42 +1,41 @@
 /* setup enzyme */
-import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-configure({ adapter: new Adapter() })
+import {configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure( {adapter: new Adapter()} );
 
 /* setup jsdom */
-var jsdom = require('jsdom')
-const { JSDOM } = jsdom
-const window = new JSDOM('').window
-global.window = window
-global.document = window.document
+var jsdom = require( 'jsdom' );
+const {JSDOM} = jsdom;
+const window = new JSDOM( '' ).window;
+global.window = window;
+global.document = window.document;
 
 import React from 'react'
 import renderer from 'react-test-renderer'
-import { shallow } from 'enzyme'
-import SortableList from '../SortableList'
+import {shallow} from 'enzyme'
+import SortableItems from '../SortableItems'
 
-test('SortableList renders correctly and matches snapshot', () => {
-  let colors = ['Red', 'Green', 'Blue', 'Yellow', 'Black', 'White', 'Orange']
+test( 'SortableItems render matches snapshot', () => {
+	let colors = ['Red', 'Green', 'Blue', 'Yellow', 'Black', 'White', 'Orange'];
 
-  const component = renderer.create(
-    <SortableList data={colors} />
-  )
+	const component = renderer.create(
+		<SortableItems data={colors}/>
+	);
 
-  let tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
-})
+	let tree = component.toJSON();
+	expect( tree ).toMatchSnapshot();
+} );
 
-test('SortableList renders the correct elements and props', () => {
-  let colors = ['Red', 'Green', 'Blue', 'Yellow', 'Black', 'White', 'Orange']
+test( 'SortableItems renders the correct elements', () => {
+	let colors = ['Red', 'Green', 'Blue', 'Yellow', 'Black', 'White', 'Orange'];
 
-  const wrapper = shallow(
-    <SortableList data={colors} />
-  )
+	const wrapper = shallow(
+		<SortableItems data={colors}/>
+	);
 
-  expect(wrapper.instance().props.data).toEqual(colors)
+	expect( wrapper.instance().props.data ).toEqual( colors );
 
-  expect(wrapper.find('ul').length).toEqual(1)
-  expect(wrapper.find('li').length).toEqual(colors.length)
-  expect(wrapper.find('li').first().prop('draggable')).toEqual('true');
-  expect(wrapper.find('li').first().prop('data-id')).toEqual(0);
-})
+	expect( wrapper.children( '.sortable-items--item' ).length ).toEqual( colors.length );
+	expect( wrapper.children( '.sortable-items--item' ).first().prop( 'draggable' ) ).toEqual( 'true' );
+} );
