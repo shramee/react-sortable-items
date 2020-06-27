3564421af7dddf1b78425671a5cfa4438f830543
Shramee Srivastav <shramee.srivastav@gmail.com>
2020-06-27 18:17:54 +0800

    Sorting and onChange callback tests

--- src/__tests__/SortableList.spec.js
+++ src/__tests__/SortableList.spec.js
@@ -16,9 +16,9 @@ import renderer from 'react-test-renderer'
 import {shallow} from 'enzyme'
 import SortableItems from '../SortableItems'
 
-test( 'SortableItems render matches snapshot', () => {
-	let colors = ['Red', 'Green', 'Blue', 'Yellow', 'Black', 'White', 'Orange'];
+const colors = ['Red', 'Green', 'Blue', 'Yellow', 'Black', 'White', 'Orange'];
 
+test( 'SortableItems render matches snapshot', () => {
 	const component = renderer.create(
 		<SortableItems data={colors}/>
 	);
@@ -28,21 +28,15 @@ test( 'SortableItems render matches snapshot', () => {
 } );
 
 test( 'SortableItems renders the correct elements', () => {
-	let colors = ['Red', 'Green', 'Blue', 'Yellow', 'Black', 'White', 'Orange'];
-
 	const wrapper = shallow(
 		<SortableItems data={colors}/>
 	);
-
-	expect( wrapper.instance().props.data ).toEqual( colors );
 
 	expect( wrapper.children( '.sortable-items--item' ).length ).toEqual( colors.length );
 	expect( wrapper.children( '.sortable-items--item' ).first().prop( 'draggable' ) ).toEqual( 'true' );
 } );
 
 test( 'SortableItems wrapProps className', () => {
-	let colors = ['Red', 'Green', 'Blue', 'Yellow', 'Black', 'White', 'Orange'];
-
 	const wrapper = shallow(
 		<SortableItems data={colors} wrapProps={{className: 'my-sortable-stuff',}}/>
 	);
@@ -51,11 +45,54 @@ test( 'SortableItems wrapProps className', () => {
 } );
 
 test( 'SortableItems itemProps className', () => {
-	let colors = ['Red', 'Green', 'Blue', 'Yellow', 'Black', 'White', 'Orange'];
-
 	const wrapper = shallow(
-		<SortableItems data={colors} itemProps={{className: 'my-sortable-items',}}/>
+		<SortableItems data={colors} itemProps={{className: 'my-sortable-item',}}/>
 	);
 
-	expect( wrapper.children( '.my-sortable-items' ).length ).toEqual( colors.length );
+	expect( wrapper.children( '.my-sortable-item' ).length ).toEqual( colors.length );
+} );
+
+test( 'SortableItems data sorting', () => {
+	const wrapper = shallow(
+		<SortableItems data={colors}/>
+	);
+
+	expect( wrapper.find( '.sortable-items--item' ).at( 0 ).text() ).toEqual( colors[0] );
+
+	wrapper.instance().sortData( 5, 0 );
+
+	expect( wrapper.find( '.sortable-items--item' ).at( 0 ).text() ).toEqual( colors[5] );
+
+	wrapper.instance().sortData( 0, 5 );
+
+	wrapper.instance().sortData( 1, 5 );
+
+	expect( wrapper.find( '.sortable-items--item' ).at( 5 ).text() ).toEqual( colors[1] );
+
+} );
+
+test( 'SortableItems data onChange callback', () => {
+
+	let
+		delim = ', ',
+		colorsArr = [...colors],
+		colorsList = colorsArr.join( delim );
+
+	const onChange = items => colorsList = items.join( delim );
+
+	const wrapper = shallow(
+		<SortableItems data={colors} onChange={onChange}/>
+	);
+
+	expect( colorsList ).toEqual( colorsArr.join( delim ) );
+
+	// Operation in component
+	wrapper.instance().sortData( 5, 0 );
+
+	// Simulate operation on our array
+	colorsArr.splice( 0, 0, colorsArr.splice( 5, 1 )[0] );
+
+	// onChange callback should change colorsList
+	expect( colorsList ).toEqual( colorsArr.join( delim ) );
+
 } );
